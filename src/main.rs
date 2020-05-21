646b67068cceda8f4dd813632cc6fffa3f74ce87
Dandandan <danielheres@gmail.com>
2020-05-21 12:05:56 +0200

    Add toml support

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
 use sqlparser::dialect::AnsiDialect;
 use sqlparser::parser::Parser;
-use toml::Value;
+use std::fs;
 use walkdir::{Error, WalkDir};
 
+use serde_derive::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct PowerSqlConfig {
+    project: Project,
+}
+#[derive(Deserialize, Debug)]
+struct Project {
+    name: String,
+    models: Vec<String>,
+}
+
 pub fn main() -> Result<(), Error> {
-    let value = "foo = 'bar'".parse::<Value>().unwrap();
-    print!("{:?}", value);
+    let root_dir = "examples/project_1/";
+
+    // Load project
+    let contents = fs::read_to_string(format!("{}{}", root_dir, "powersql.toml"))
+        .expect("No powersql.toml file found");
+
+    let config: PowerSqlConfig = toml::from_str(&contents).unwrap();
+    println!("{:?}", config);
 
     let dialect = AnsiDialect {};
 
-    for entry in WalkDir::new("examples") {
-        let entry = entry?;
-        if let Some(abc) = entry.path().extension() {
-            {
-                if abc == "sql" {
-                    print!("{:?}", entry.path());
+    for dir in config.project.models {
+        for entry in WalkDir::new(format!("{}/{}", root_dir, dir)) {
+            let entry = entry?;
+            if let Some(abc) = entry.path().extension() {
+                {
+                    if abc == "sql" {
+                        println!("{:?}", entry.path());
+                    }
                 }
             }
         }
