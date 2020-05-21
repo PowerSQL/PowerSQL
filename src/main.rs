42ed8087afc2d3636fa9312cea80f934a272dc90
Dandandan <danielheres@gmail.com>
2020-05-21 12:21:28 +0200

    Read model files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use sqlparser::dialect::AnsiDialect;
 use sqlparser::parser::Parser;
 use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
 use walkdir::{Error, WalkDir};
 
 use serde_derive::Deserialize;
@@ -27,27 +29,29 @@ pub fn main() -> Result<(), Error> {
 
     let dialect = AnsiDialect {};
 
+    let mut models = vec![];
+
     for dir in config.project.models {
         for entry in WalkDir::new(format!("{}/{}", root_dir, dir)) {
-            let entry = entry?;
+            let entry = entry.unwrap();
             if let Some(abc) = entry.path().extension() {
                 {
                     if abc == "sql" {
-                        println!("{:?}", entry.path());
+                        let e = entry.clone();
+                        models.push(e);
                     }
                 }
             }
         }
     }
 
-    let sql = "SELECT a, b, 123, myfunc(b) \
-           FROM table_1 \
-           WHERE a > b AND b < 100 \
-           ORDER BY a DESC, b";
+    for m in models {
+        let sql = fs::read_to_string(m.path()).unwrap();
 
-    let ast = Parser::parse_sql(&dialect, sql.to_string()).unwrap();
+        let ast = Parser::parse_sql(&dialect, sql).unwrap();
 
-    println!("AST: {:?}", ast);
+        println!("AST: {:?}", ast);
+    }
 
     Ok(())
 }
