6b79ff64157b2729aa944404d9fe279d4cc9c3e4
Dandandan <danielheres@gmail.com>
2020-05-23 17:24:14 +0200

    Start splitting out into functions, add test for dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use rayon::prelude::*;
 use serde_derive::Deserialize;
-use sqlparser::ast::Query;
+use sqlparser::ast::{Cte, Query, SetExpr, TableFactor};
 use sqlparser::parser::Parser;
-use sqlparser::tokenizer::{Token, Tokenizer, Whitespace, Word};
+use sqlparser::tokenizer::Tokenizer;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs;
+use walkdir::DirEntry;
 use walkdir::WalkDir;
 
 #[derive(Deserialize, Debug)]
@@ -21,7 +23,9 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 enum Command {
     Check,
+    Run,
     Lint,
+    Docs,
 }
 
 #[derive(Debug, StructOpt)]
@@ -49,6 +53,70 @@ impl sqlparser::dialect::Dialect for PowerSqlDialect {
     }
 }
 
+fn get_refs_cte(cte: &Cte, vec: &mut Vec<String>) {
+    get_refs(&cte.query, vec)
+}
+
+fn get_refs_set_expr(ctes: &SetExpr, vec: &mut Vec<String>) {
+    match ctes {
+        SetExpr::Query(q) => get_refs(q, vec),
+        SetExpr::Select(s) => s.from.iter().for_each(|x| match &x.relation {
+            TableFactor::Table { name, .. } => {
+                if name.0[0].starts_with("@") {
+                    vec.push(name.0.join("."))
+                }
+            }
+            _ => {}
+        }),
+        _ => {}
+    }
+}
+
+fn get_refs(query: &Query, vec: &mut Vec<String>) {
+    query.ctes.iter().for_each(|x| get_refs_cte(x, vec));
+    get_refs_set_expr(&query.body, vec);
+}
+
+fn load_asts(models: &Vec<DirEntry>) -> HashMap<String, Query> {
+    models
+        .par_iter()
+        .map(|x| {
+            let sql = fs::read_to_string(x.path()).unwrap();
+
+            //let ast = Parser::parse_sql(&dialect, sql).unwrap();
+            let tokens = Tokenizer::new(&PowerSqlDialect {}, &sql)
+                .tokenize()
+                .unwrap();
+            let mut parser = Parser::new(tokens);
+            // TODO handle parse error
+            let ast = parser.parse_query().unwrap();
+
+            (x.path().to_str().unwrap().to_string(), ast)
+        })
+        .collect()
+}
+
+fn get_dependencies(
+    asts: &HashMap<String, Query>,
+    mappings: &HashMap<String, &str>,
+) -> HashMap<String, Vec<String>> {
+    asts.iter()
+        .map(|(src, query)| {
+            let mut x = vec![];
+            get_refs(query, &mut x);
+            let m = x
+                .iter()
+                .map(|y| {
+                    // TODO handle errors
+                    let s = y.trim_start_matches('@');
+                    (*mappings.get(s).unwrap()).to_string()
+                })
+                .collect();
+            (src.clone(), m)
+        })
+        .collect()
+}
+
 pub fn main() -> Result<(), String> {
     let opt = Opt::from_args();
 
@@ -59,12 +127,10 @@ pub fn main() -> Result<(), String> {
         Err(x) => return Err(x.to_string()),
         Ok(conf) => conf,
     };
-
-    let dialect = PowerSqlDialect {};
     let mut models = vec![];
 
     for dir in config.project.models {
-        for entry in WalkDir::new(format!("{}", dir)) {
+        for entry in WalkDir::new(dir.to_string()) {
             let entry = entry.unwrap();
             if let Some(ext) = entry.path().extension() {
                 {
@@ -79,25 +145,82 @@ pub fn main() -> Result<(), String> {
 
     match opt.command {
         Command::Check => {
-            // Check / load
-            let asts: HashMap<String, Query> = models
-                .par_iter()
-                .map(|x| {
-                    let sql = fs::read_to_string(x.path()).unwrap();
-
-                    //let ast = Parser::parse_sql(&dialect, sql).unwrap();
-                    let tokens = Tokenizer::new(&dialect, &sql).tokenize().unwrap();
-                    let mut parser = Parser::new(tokens);
-                    let ast = parser.parse_query().unwrap();
-
-                    (x.path().to_str().unwrap().to_string(), ast)
+            // Create mappings to full name
+            let mappings: HashMap<String, &str> = models
+                .iter()
+                .flat_map(|x| {
+                    let x = x.path().to_str().unwrap();
+                    let parts: Vec<&str> = x.trim_end_matches(".sql").split('/').collect();
+                    let mut s = String::new();
+                    let mut res = Vec::with_capacity(parts.len() - 1);
+                    for &p in parts.iter().rev().take(parts.len() - 1) {
+                        s += p;
+                        res.push((s.to_string(), x));
+                    }
+                    res
                 })
                 .collect();
 
-            println!("{} models correctly loaded", asts.len());
+            let asts = load_asts(&models);
+
+            // Create list of dependencies
+            let deps: HashMap<String, Vec<String>> = get_dependencies(&asts, &mappings);
+
+            // Cycle detection
+            let mut visited_all = HashSet::new();
+            for (model, model_deps) in deps.iter() {
+                let mut visited = HashSet::new();
+                let mut stack = model_deps.clone();
+
+                visited.insert(model);
+                while let Some(x) = stack.pop() {
+                    if visited_all.contains(&x) {
+                        continue;
+                    }
+                    visited_all.insert(x.clone());
+                    let d = &deps[&x];
+
+                    for i in d.iter() {
+                        if visited.contains(i) {
+                            return Err(format!("Loop detected while checking model {}", model));
+                        }
+                        visited.insert(i);
+                    }
+                    stack.extend(d.clone());
+                }
+            }
+
+            println!("{:?}", mappings);
+            println!("{} models loaded", asts.len());
+            println!("{:?} deps", deps);
         }
+        Command::Run => unimplemented!(),
         Command::Lint => unimplemented!(),
+        Command::Docs => unimplemented!(),
     }
 
     Ok(())
+}
+
+#[test]
+fn test_dependencies() {
+    let sql = "select a from @t";
+    let tokens = Tokenizer::new(&PowerSqlDialect {}, &sql)
+        .tokenize()
+        .unwrap();
+    let mut parser = Parser::new(tokens);
+    let ast = parser.parse_query().unwrap();
+
+    let x = get_dependencies(
+        &(vec![("x".to_string(), ast)].iter().cloned().collect()),
+        &(vec![("t".to_string(), "t.sql")].iter().cloned().collect()),
+    );
+
+    assert_eq!(
+        x,
+        vec![("x".to_string(), vec!["t.sql".to_string()])]
+            .iter()
+            .cloned()
+            .collect()
+    )
 }
