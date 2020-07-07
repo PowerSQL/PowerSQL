d531aa28c4ef8b4d4615e2738eb2ba3cd22ecbc5
Daniël Heres <danielheres@gmail.com>
2020-07-07 17:41:53 +0200

    Misc improvements & simplifications (#7)

---
+++ examples/project_1/models/revenue.sql
@@ -0,0 +1,10 @@
+CREATE VIEW revenue AS
+SELECT product_id, euro
+FROM product_sales;
+CREATE MATERIALIZED VIEW rev_per_product AS
+SELECT SUM(euro) AS rev,
+    COUNT(*) quantity,
+    product_id
+FROM revenue
+GROUP BY product_id;
+--SELECT * FROM rev_per_product ORDER by quantity DESC LIMIT 100;
