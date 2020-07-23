402db7b98588d554b1edb8ad4d2811422392f4db
Dandandan <danielheres@gmail.com>
2020-07-23 22:21:46 +0200

    Change dir

--- examples/project_1/import_data/import.py
+++ examples/project_1/import_data/import.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 
 import pandas as pd
 
-df = pd.read_csv('product_sales.csv')
+df = pd.read_csv('import_data/product_sales.csv')
 engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
 df.to_sql('product_sales', engine)
