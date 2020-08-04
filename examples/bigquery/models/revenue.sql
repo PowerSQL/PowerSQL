be07e4dbdb3cf32f66353d0d8dffb0f69a5ce255
Heres, Daniel <danielheres@gmail.com>
2020-08-05 00:14:09 +0200

    Remove unneeded OR REPLACE

--- examples/bigquery/models/revenue.sql
+++ examples/bigquery/models/revenue.sql
@@ -1,7 +1,6 @@
-CREATE OR REPLACE VIEW revenue AS
 SELECT CAST('my_product' AS STRING) product_id,
     1.0 AS euro;
-CREATE OR REPLACE TABLE rev_per_product AS
+CREATE TABLE rev_per_product AS
 SELECT SUM(euro) AS rev,
     COUNT(*) quantity,
     product_id
